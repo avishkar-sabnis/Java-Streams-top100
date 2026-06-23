a2674703064b837c8a53561d50a9d16243a93f82
Avishkar Sabnis <32098673+avishkar-sabnis@users.noreply.github.com>
2026-06-23 17:59:41 +0530

    input -->[Employee{employeeName='Suresh', employeeId=1, employeeSalary=100000.1}, Employee{employeeName='Nikhil', employeeId=2, employeeSalary=26500.1}, Employee{employeeName='Vijay', employeeId=3, employeeSalary=582200.1}, Employee{employeeName='Neha', employeeId=4, employeeSalary=1052660.1}]
    output -->[abp, fko, jjo, zxy]
              10 12 12 14 19 34 5
              19 12 14 10 34 101

---
+++ src/SumOfAll_Integers.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class SumOfAll_Integers {
+
+    public static void main(String[] args) {
+
+        int[] arr = {19,12,14,10,12,34};
+
+        List<String> stringList = Arrays.asList("abp","fko","zxy","jjo");
+        List<String> list = stringList.stream().sorted(Comparator.comparing(String::toString)).toList();
+        System.out.println(list);
+
+        int[] sortInAscending = Arrays.stream(arr).boxed()
+                .sorted(Comparator.comparingInt((x-> (int) x)))
+                .mapToInt(Integer::intValue).toArray();
+        for(int rem:sortInAscending){
+            System.out.print(rem+" ");
+        }
+
+        long removeDuplicateIntegersAndGetCount = Arrays.stream(Arrays.stream(arr).
+                distinct().toArray()).count();
+        System.out.println(removeDuplicateIntegersAndGetCount);
+
+        int[] removeDuplicateIntegers = Arrays.stream(arr).
+                distinct().toArray();
+        for(int rem:removeDuplicateIntegers){
+            System.out.print(rem+" ");
+        }
+        int listOfEvenNumbers = Arrays.stream(arr).
+                reduce((x,y)->x+y).getAsInt();
+        System.out.println(listOfEvenNumbers);
+
+
+    }
+
+}
